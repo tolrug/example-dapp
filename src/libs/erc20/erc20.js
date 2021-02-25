8a8229f4b2640e4ab0d1b2a1020a45d2f255e9cd
Kaleb Day <k.day@uqconnect.edu.au>
2021-02-25 22:26:09 +1000

    Api get request to fetch all owned tokens by account

--- src/libs/erc20/erc20.js
+++ src/libs/erc20/erc20.js
@@ -1,49 +1,61 @@
-import { useEffect, useState, createContext, useContext } from 'react'
-import { useNetwork } from '../web3'
-import ERC20ABI from './erc20.json';
+import { useEffect, useState, createContext, useContext } from "react";
+import { useNetwork } from "../web3";
+import ERC20ABI from "./erc20.json";
 
 const Context = createContext({});
 
-const useContract = () => useContext(Context)
+const useContract = () => useContext(Context);
 
-const Provider = 
-	({
-		children
-	}) => {
+const Provider = ({ children }) => {
+  // Here we use our hooks to get access to some juicy stuff
+  let { web3 } = useNetwork();
+  let [contract, setContract] = useState();
+  const [account, setAccount] = useState("");
 
-		// Here we use our hooks to get access to some juicy stuff
-		let { web3 } = useNetwork()
-		let [contract, setContract] = useState()
+  // Initialise the contract on component mount
+  useEffect(() => {
+    if (web3) {
+      // An ethereum ABI, is simply a JSON representation of the ethereum smart contract
+      // It details all the expected usable functions
+      // To instantiate a useable contract we parse in the ABI as well as an Ethereum address.
+      // This is the address corresponding to the location of the smart contract store on ethereum
+      // The example address we gave is to the Pickle token
+      const _contract = new web3.eth.Contract(
+        ERC20ABI,
+        "0x429881672B9AE42b8EbA0E26cD9C73711b891Ca5"
+      );
+      setContract(_contract);
+    }
+  }, [web3]); // eslint-disable-line
 
-		// Initialise the contract on component mount
-		useEffect(() => {
-			if(web3){
-				// An ethereum ABI, is simply a JSON representation of the ethereum smart contract
-				// It details all the expected usable functions
-				// To instantiate a useable contract we parse in the ABI as well as an Ethereum address.
-				// This is the address corresponding to the location of the smart contract store on ethereum
-				// The example address we gave is to the Pickle token
-				const _contract = new web3.eth.Contract(ERC20ABI, '0x429881672B9AE42b8EbA0E26cD9C73711b891Ca5')
-				setContract(_contract)
-			}
-		}, [web3]) // eslint-disable-line
+  const fetchPickleBalance = async (address) => {
+    return await contract.methods.balanceOf(address).call();
+  };
 
+  const getTokens = async (address) => {
+    return await contract.methods.symbol().call();
+  };
 
-		const fetchPickleBalance = async (address) => {
-			return await contract.methods.balanceOf(address).call()
-		}
+  const getAccount = async () => {
+    const accounts = await web3.eth.getAccounts();
+    return accounts[0];
+  };
 
-		return <Context.Provider 
-			value={{
-				// expose more methods here to interact with the contract
-				fetchPickleBalance
-			}}
-			>
-			{children}
-		</Context.Provider>
-	};
+  return (
+    <Context.Provider
+      value={{
+        // expose more methods here to interact with the contract
+        fetchPickleBalance,
+        getTokens,
+        getAccount,
+      }}
+    >
+      {children}
+    </Context.Provider>
+  );
+};
 
 export default {
-	Provider,
-	useContract
-}
+  Provider,
+  useContract,
+};
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,65 +1,109 @@
-import React, { useState } from 'react';
-import styled from 'styled-components'
-import { useAccount } from '../libs/web3'
-import { Input, Button} from 'antd'
-import ERC20 from '../libs/erc20/erc20';
-import Web3 from 'web3';
+import React, { useState } from "react";
+import styled from "styled-components";
+import { useAccount } from "../libs/web3";
+import { Input, Button } from "antd";
+import ERC20 from "../libs/erc20/erc20";
+import Web3 from "web3";
+import axios from "axios";
 
-const Instructions = styled(({
-    className
-}) => {
-    const { fetchPickleBalance } = ERC20.useContract();
+const Instructions = styled(({ className }) => {
+  const { fetchPickleBalance, getTokens, getAccount } = ERC20.useContract();
 
-    const [address, setAddress] = useState('')
-    const [balance, setBalance] = useState('?')
+  const [address, setAddress] = useState("");
+  const [balance, setBalance] = useState("?");
+  const [tokens, setTokens] = useState();
+  const [account, setAccount] = useState("");
+  const [wallet, setWallet] = useState();
 
-    const handleClick = async (e) => {
-        e.preventDefault();
-        const balance = await fetchPickleBalance(address);
-        // Since we dont store decimals on the block chain we use wei to store our values
-        // wei is the smallest unit of ethereum.
-        // it just adds a bunch of zeros to whatever value is stored (10^-18)
-        // fromWei is just a handy util to convert it back to something we understand
-        setBalance(Web3.utils.fromWei(balance))
-    }
+  const testAccount = "0x4e34e4cc38fa422a1a23eaab4e7a8d0af3bcbe9f";
 
-    const handleChange = (e) => {
-        e.preventDefault();
-        setAddress(e.target.value);
-    }
+  const handleClick = async (e) => {
+    e.preventDefault();
+    const balance = await fetchPickleBalance(address);
+    // Since we dont store decimals on the block chain we use wei to store our values
+    // wei is the smallest unit of ethereum.
+    // it just adds a bunch of zeros to whatever value is stored (10^-18)
+    // fromWei is just a handy util to convert it back to something we understand
+    setBalance(Web3.utils.fromWei(balance));
+  };
 
-    return (
-        <>
-            <h1>
-                So you're telling me I'm connected to the ethereum blockchain :o
-            </h1>
-            <h2>Check ./libs/erc20 to see an example of how to interact with smart contracts</h2>
+  const handleChange = (e) => {
+    e.preventDefault();
+    setAddress(e.target.value);
+  };
 
-            <p>
-                Try enter <a href="https://etherscan.io/address/0x2252A85e520fE2f29E0be62104D8551B32649C66">0xd1dE80930227C56eE8bB2049e4D36bFf4161163E</a> into the field below and hit submit
-            </p>
+  const getAccountFunc = async (e) => {
+    e.preventDefault();
+    const account = await getAccount();
+    setAccount(account);
+  };
 
-            <div>
-                <Input placeholder="Enter in an ethereum address" value={address} onChange={handleChange}/>
-                <Button onClick={handleClick}>Get Pickle Balance</Button>
-            </div>
-            <p>Balance: {balance}</p>
-        </>
-    )
-})
-``
+  const getTokensList = (e) => {
+    e.preventDefault();
 
-export default styled(
-	({
-		className
-	}) => {
-		const { status } = useAccount()
+    console.log(account);
 
-		return status === 'CONNECTED'
-            ? 	<Instructions />
-			: <></>
-	})
-	`
-		display: flex;
-		align-items: center;
-	`
+    axios({
+      method: "get",
+      url: `https://web3api.io/api/v1/addresses/${testAccount}/tokens`,
+      headers: { "x-api-key": "UAK7797b1c5e7179fc6e1c9f81d469f5067" },
+    }).then((res) => {
+      console.log(res.data.payloads.records);
+    });
+  };
+
+  return (
+    <>
+      <h1>So you're telling me I'm connected to the ethereum blockchain :o</h1>
+      <h2>
+        Check ./libs/erc20 to see an example of how to interact with smart
+        contracts
+      </h2>
+
+      <p>
+        Try enter{" "}
+        <a href="https://etherscan.io/address/0x2252A85e520fE2f29E0be62104D8551B32649C66">
+          0xd1dE80930227C56eE8bB2049e4D36bFf4161163E
+        </a>{" "}
+        into the field below and hit submit
+      </p>
+
+      <div>
+        <Input
+          placeholder="Enter in an ethereum address"
+          value={address}
+          onChange={handleChange}
+        />
+        <Button onClick={handleClick}>Get Pickle Balance</Button>
+      </div>
+      <p>Balance: {balance}</p>
+      <br />
+      <br />
+
+      <h1>Now we can display all the owned tokens</h1>
+      <button onClick={getAccountFunc}>Get Tokens</button>
+      <p>
+        Account: <br />
+        {account}
+      </p>
+
+      <br />
+      <h1>Or get our wallet</h1>
+      <button onClick={getTokensList}>Get Wallet</button>
+      <p>
+        Wallet:
+        <br />
+        {wallet}
+      </p>
+    </>
+  );
+})``;
+
+export default styled(({ className }) => {
+  const { status } = useAccount();
+
+  return status === "CONNECTED" ? <Instructions /> : <></>;
+})`
+  display: flex;
+  align-items: center;
+`;
